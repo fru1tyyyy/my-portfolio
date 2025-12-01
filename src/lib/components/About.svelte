44653b5ed1820dfa167da0d6dc74fd295de92ab1
Been <161136564+fru1tyyyy@users.noreply.github.com>
2025-12-01 20:38:47 +0800

    Add files via upload

---
+++ src/lib/components/About.svelte
@@ -0,0 +1,23 @@
+<div class="about-container">
+    <div class="about">
+        <img src="/img/pfp.jpg" alt="pfp" class="pfp">
+        <div class="intro">
+            <h1>Yu Been</h1>
+            <p>:p</p>
+        </div>
+    </div>
+    <nav class="navbar">
+    <ul class="nav-links">
+      <li><a href="https://linkedin.com/in/been-yu-804389306" target="_blank"><i class="fab fa-linkedin"></i> LinkedIn</a></li>
+      <li><a href="https://github.com/fru1tyyyy" target="_blank"><i class="fab fa-github"></i> GitHub</a></li>
+      <li><a href="https://instagram.com/yubeen721" target="_blank"><i class="fab fa-instagram"></i> Instagram</a></li>
+    </ul>
+  </nav>
+  <p>I am passionate about coding and problem-solving, with a strong foundation in programming, web development, and software design. I enjoy learning new technologies and building practical projects. My goal is to become a full-stack developer, creating efficient, scalable, and user-friendly solutions, driven by curiosity and the desire to contribute to impactful projects.</p>
+</div>
+<style>
+  .about-container {
+    padding-top: 100px;
+    text-align: left; 
+  }
+</style>
---
+++ src/lib/components/Info.svelte
@@ -0,0 +1,18 @@
+<div class="project">
+  <h2>~ My Location</h2>
+  <p>Malaysia</p>
+  <h2>~ My Hobby</h2>
+  <p>Gaming, Coding, Tech Stuff, Cubing</p>
+  <h2>~ My Education</h2>
+  <p>Bachelor Software Engineering - Sunway University</p>
+  <h2>~ My Skills</h2>
+  <p>Web Development, Game Development, Computer Hardware</p>
+  <h2>~ My Languages</h2>
+  <p>English, Chinese, Malay</p>
+</div>
+<style>
+  .project {
+    padding: 50px;
+    text-align: left;
+  }
+</style>
